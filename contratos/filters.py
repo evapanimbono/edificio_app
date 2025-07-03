00034dcf44477a71779e5d126d39c899c61b30b2
Eva Panimbono <evalpanimbono@gmail.com>
2025-07-03 11:10:00 +0200

    Agrega filtros por estado, apartamento y fecha en vistas de contratos

--- contratos/filters.py
+++ contratos/filters.py
@@ -3,15 +3,18 @@ from .models_mensualidades import Mensualidad
 from .models import Contrato
 
 class MensualidadFilter(django_filters.FilterSet):
+    estado = django_filters.CharFilter(field_name="estado")
+    fecha_inicio = django_filters.DateFilter(field_name="fecha_inicio", lookup_expr="gte")
+    fecha_vencimiento = django_filters.DateFilter(field_name="fecha_vencimiento", lookup_expr="lte")
+    contrato = django_filters.NumberFilter(field_name="contrato_id")
+
     class Meta:
         model = Mensualidad
-        fields = {
-            'contrato': ['exact'],
-            'estado': ['exact'],
-            'fecha_vencimiento': ['exact', 'lte', 'gte'],
-        }
+        ields = ['estado', 'fecha_inicio', 'fecha_vencimiento', 'contrato']
 
 class ContratosFilter(django_filters.FilterSet):
+    apartamento = django_filters.NumberFilter(field_name="apartamento_id")
+
     class Meta:
         model = Contrato
-        fields = ['arrendatario', 'apartamento']        
+        fields = ['apartamento']        
--- contratos/views.py
+++ contratos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework import generics
 from rest_framework.views import APIView
@@ -7,34 +8,45 @@ from rest_framework.permissions import IsAuthenticated
 
 from .models import Contrato
 from .serializers_contratos import ContratoSerializer
-from .filters import ContratosFilter
+from .filters import ContratosFilter, MensualidadFilter
 
 from .models_mensualidades import Mensualidad
 from .serializers_mensualidades import MensualidadSerializer
 from .filters import MensualidadFilter
 
-from django_filters.rest_framework import DjangoFilterBackend
-
+#Vista de contatos filtrada por tipo de usuario
 class ListaContratosAPIView(generics.ListAPIView):
+    # Arrendatario: lista de contratos donde es arrendatario
+    # Arrendador: lista de contratos de los apartamentos que administra
     serializer_class = ContratoSerializer 
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ContratosFilter
 
     def get_queryset(self):
         user = self.request.user
 
         if user.tipo_usuario == 'arrendatario':
             return Contrato.objects.filter(arrendatario=user)
+        
         elif user.tipo_usuario == 'arrendador':
             edificios = user.edificios_asignados.values_list('edificio_id', flat=True)
             return Contrato.objects.filter(apartamento__edificio_id__in=edificios)
+        
         if user.is_superuser:
             return Contrato.objects.all()
 
         return Contrato.objects.none()
 
+#Vista de mensualidades filtrada por tipo de usuario
 class ListaMensualidadesAPIView(generics.ListAPIView):
+    # Arrendatario: lista de mensualidades de sus contratos
+    # Arrendador: lista de mensualidades de los apartamentos que administra
+
     serializer_class = MensualidadSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = MensualidadFilter
 
     def get_queryset(self):
         user = self.request.user
@@ -45,6 +57,7 @@ class ListaMensualidadesAPIView(generics.ListAPIView):
         elif user.tipo_usuario == 'arrendador':
             edificios = user.edificios_asignados.values_list('edificio_id', flat=True)
             return Mensualidad.objects.filter(contrato__apartamento__edificio_id__in=edificios)
+        
         if user.is_superuser:
             return Mensualidad.objects.all()
 
