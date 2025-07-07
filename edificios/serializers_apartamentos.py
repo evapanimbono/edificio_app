950f2de526ec6bda4eb336577b3016a7d689bc28
Eva Panimbono <evalpanimbono@gmail.com>
2025-07-07 14:18:11 +0200

    Agrega serializers para Apartamento: lista, detalle y crear/editar

--- edificios/serializers_apartamentos.py
+++ edificios/serializers_apartamentos.py
@@ -2,7 +2,21 @@
 from rest_framework import serializers
 from .models_apartamentos import Apartamento
 
-class ApartamentoSerializer(serializers.ModelSerializer):
+class ApartamentoListSerializer(serializers.ModelSerializer):
+    edificio_nombre = serializers.CharField(source='edificio.nombre', read_only=True)
+
+    class Meta:
+        model = Apartamento
+        fields = ['id', 'edificio', 'edificio_nombre', 'numero_apartamento', 'piso', 'estado']
+
+class ApartamentoDetailSerializer(serializers.ModelSerializer):
+    edificio_nombre = serializers.CharField(source='edificio.nombre', read_only=True)
+
     class Meta:
         model = Apartamento
         fields = '__all__'
+
+class ApartamentoCrearEditarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Apartamento
+        fields = ['edificio', 'numero_apartamento', 'piso', 'habitaciones', 'banos', 'descripcion', 'estado']
