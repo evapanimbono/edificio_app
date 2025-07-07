81c0ee642a185447f1802343577b662477bc0e26
Eva Panimbono <evalpanimbono@gmail.com>
2025-07-07 12:18:36 +0200

    Finaliza implementacion de mensualidades y contratos: filtros mejorados,logs,permisos y vistas completas

--- contratos/filters.py
+++ contratos/filters.py
@@ -3,18 +3,30 @@ from .models_mensualidades import Mensualidad
 from .models import Contrato
 
 class MensualidadFilter(django_filters.FilterSet):
-    estado = django_filters.CharFilter(field_name="estado")
-    fecha_inicio = django_filters.DateFilter(field_name="fecha_inicio", lookup_expr="gte")
-    fecha_vencimiento = django_filters.DateFilter(field_name="fecha_vencimiento", lookup_expr="lte")
+    estado = django_filters.ChoiceFilter(field_name="estado", choices=Mensualidad.ESTADO_CHOICES)
+    fecha_generacion_desde = django_filters.DateFilter(field_name="fecha_generacion", lookup_expr="gte")
+    fecha_generacion_hasta = django_filters.DateFilter(field_name="fecha_generacion", lookup_expr="lte")
+    fecha_vencimiento_desde = django_filters.DateFilter(field_name="fecha_vencimiento", lookup_expr="gte")
+    fecha_vencimiento_hasta = django_filters.DateFilter(field_name="fecha_vencimiento", lookup_expr="lte")
     contrato = django_filters.NumberFilter(field_name="contrato_id")
+
+    # Campos relacionales útiles
+    apartamento = django_filters.NumberFilter(field_name="contrato__apartamento_id")
+    usuario = django_filters.NumberFilter(field_name="contrato__arrendatario_id")
+    edificio = django_filters.NumberFilter(field_name="contrato__apartamento__edificio_id")
 
     class Meta:
         model = Mensualidad
-        fields = ['estado', 'fecha_inicio', 'fecha_vencimiento', 'contrato']
-
+        fields = [
+            'estado', 'fecha_generacion_desde', 'fecha_generacion_hasta',
+            'fecha_vencimiento_desde', 'fecha_vencimiento_hasta',
+            'contrato', 'apartamento', 'usuario', 'edificio'
+        ]
 class ContratosFilter(django_filters.FilterSet):
     apartamento = django_filters.NumberFilter(field_name="apartamento_id")
+    usuario = django_filters.NumberFilter(field_name="arrendatario_id")
+    edificio = django_filters.NumberFilter(field_name="apartamento__edificio_id")
 
     class Meta:
         model = Contrato
-        fields = ['apartamento']        
+        fields = ['apartamento', 'usuario', 'edificio']        
